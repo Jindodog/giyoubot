96c7d747a250edb6b1c03f841e7665edd1b82794
Jindodog <contlor33@naver.com>
2021-05-15 13:38:32 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -161,6 +161,10 @@ async def 재생(ctx, *, msg):
              await ctx.message.channel.send(embed=embed)
     
     if not vc.is_playing():
+        
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        
         global entireText
         YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
